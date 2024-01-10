572053a6f0a9e60b060950a5e009c15911fb1603
kkukuruz <116309607+kkukuruz@users.noreply.github.com>
2024-01-10 16:52:23 +0300

    Add files via upload

---
+++ lab01/02/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace lab01
+{
+    public class Rectangle
+    {
+        private double side1, side2;
+
+        public Rectangle(double sideA, double sideB)
+        {
+            side1 = sideA;
+            side2 = sideB;
+        }
+
+        private double CalculateArea()
+        {
+            return side1 * side2;
+        }
+
+        private double CalculatePerimeter()
+        {
+            return 2 * (side1 + side2);
+        }
+
+        public double Area
+        {
+            get { return CalculateArea(); }
+        }
+        public double Perimeter
+        {
+            get { return CalculatePerimeter(); }
+        }
+    }
+
+    class Programm
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите длину стороны A: ");
+            double sideA = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину стороны B: ");
+            double sideB = double.Parse(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(sideA, sideB);
+
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.Area}");
+            Console.WriteLine($"Периметр прямоугольника: {rectangle.Perimeter}");
+        }
+    }
+}
---
+++ lab01/03/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace lab01
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Figure
+    {
+        private Point[] points;
+        public string Name { get; set; }
+
+        public Figure(params Point[] points)
+        {
+            if (points.Length < 3 || points.Length > 5)
+                throw new ArgumentException("Фигура должна состоять из 3-5 точек.");
+
+            this.points = points;
+            Name = "";
+        }
+
+        public double LengthSide(Point A, Point B)
+        {
+            return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+        }
+        public void PerimeterCalculator()
+        {
+            double perimeter = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                int nextIndex = (i + 1) % points.Length;
+                perimeter += LengthSide(points[i], points[nextIndex]);
+            }
+
+            Console.WriteLine($"Название фигуры: {Name}");
+            Console.WriteLine($"Периметр фигуры: {perimeter}");
+
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(0, 1);
+            Point p3 = new Point(1, 0);
+
+            Figure triangle = new Figure(p1, p2, p3);
+            triangle.Name = "Треугольник";
+            triangle.PerimeterCalculator();
+        }
+}
+}
