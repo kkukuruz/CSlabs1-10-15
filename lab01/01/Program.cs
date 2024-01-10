0900bb6a5be48f4d7409290d41e4af1a5dcca256
kkukuruz <116309607+kkukuruz@users.noreply.github.com>
2024-01-10 16:50:26 +0300

    Add files via upload

---
+++ lab01/01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace lab01;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Минимальное значение int: {int.MinValue}");
+        Console.WriteLine($"Максимальное значение int: {int.MaxValue}");
+        Console.WriteLine($"Минимальное значение double: {double.MinValue}");
+        Console.WriteLine($"Максимальное значение double: {double.MaxValue}");
+        Console.WriteLine($"Минимальное значение byte: {byte.MinValue}");
+        Console.WriteLine($"Максимальное значение byte: {byte.MaxValue}");
+
+    }
+}
